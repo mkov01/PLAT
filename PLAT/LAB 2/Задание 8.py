55b392610f44dd0fb39fa5f0d3617cdcf38fca96
mkov01 <sasha.malkov.18@bk.ru>
2026-09-09 13:35:18 +0500

    Delete PLAT directory

--- PLAT/LAB 2/Задание 8.py
+++
@@ -1,8 +0,0 @@
-total_seconds = int(input("Введите количество секунд: "))
-
-hours = total_seconds // 3600
-minutes = (total_seconds % 3600) // 60
-seconds = total_seconds % 60
-
-print("Результат в формате часы:минуты:секунды:")
-print(hours, minutes, seconds, sep=":")
--- PLAT/задание №2.py
+++
@@ -1,8 +0,0 @@
-length = float(input("Введите длину прямоугольника: "))
-width = float(input("Введите ширину прямоугольника: "))
-
-area = length * width
-perimeter = 2 * (length + width)
-
-print("Площадь прямоугольника:", area)
-print("Периметр прямоугольника:", perimeter)
